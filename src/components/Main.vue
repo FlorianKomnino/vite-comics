2eefdbfb8a0892abcde323e3c32fb22eb68dae0e
FlorianKomnino <florian.komnino@outlook.it>
2023-01-09 16:36:18 +0100

    created Main structure

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,9 +4,40 @@
 
 <template>
 
+    <div class="contentGoesHere">
+        <h2>
+            Content goes here
+        </h2>
+    </div>
+
+    <div class="blueBar">
+        <h2>
+            Blue Bar content goes here
+        </h2>
+    </div>
 
 </template>
 
 <style scoped>
+div.contentGoesHere {
+    background-color: #1c1c1c;
+}
 
+div.blueBar {
+    background-color: #0282f9;
+}
+
+div.contentGoesHere,
+div.blueBar {
+    width: 100vw;
+    height: 130px;
+    margin: 0 auto;
+    padding: 0;
+    display: flex;
+}
+
+h2 {
+    color: white;
+    margin: auto;
+}
 </style>
