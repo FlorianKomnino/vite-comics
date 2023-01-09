7876baa5f300eae2f1effb56e8d8ee5767cc2cdc
FlorianKomnino <florian.komnino@outlook.it>
2023-01-09 16:19:39 +0100

    working on Header, added logo and ul

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,10 +3,46 @@
 </script>
 
 <template>
+    <div class="container">
+        <div class="primary-logo">
+            <img src="../assets/img/dc-logo.png" alt="DC Logo">
+        </div>
 
+        <nav>
+            <ul>
+                <li>characters</li>
+                <li>characters</li>
+                <li>characters</li>
+                <li>characters</li>
+                <li>characters</li>
+                <li>characters</li>
+            </ul>
+        </nav>
+    </div>
 
 </template>
 
 <style scoped>
+ul {
+    display: flex;
+}
 
+li {
+    list-style-type: none;
+    padding: 10px;
+}
+
+img {
+    height: 100%;
+}
+
+div.primary-logo {
+    height: 70px;
+}
+
+.container {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+}
 </style>
